05d4c8b5ac0ce87d33aacfb68b2bedf2b64ef1ed
florianthepro <florianthepro@icloud.com>
2026-03-26 10:11:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ mkdir -p ~/media/music
 mkdir -p ~/media/video #mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
 
-mkdir -p ~/docker/jellyfin-enhanced-setup
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/jellyfin-enhanced-setup/compose.yaml -o ~/docker/jellyfin-enhanced-setup/compose.yaml
-sed -i "s|username|$username|g" ~/docker/jellyfin-enhanced-setup/compose.yaml
+mkdir -p ~/docker/jellyfin
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/jellyfin/compose.yaml -o ~/docker/jellyfin/compose.yaml
+sed -i "s|username|$username|g" ~/docker/jellyfin/compose.yaml
 
 #mkdir -p ~/docker/sonarr
 #curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
