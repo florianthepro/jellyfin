8143a149ac1c388a1b2f954d6a56fb4dfbf28dc3
florianthepro <florianthepro@icloud.com>
2026-03-26 11:56:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,11 @@ username="$(whoami)"
 userid="$(id -u)"
 groupid="$(id -g)"
 userpass="Password123!"
-
-#apt update
-#apt upgrade
-
+apt update
+apt upgrade
 mkdir -p ~/media/music
-mkdir -p ~/media/video #mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
+mkdir -p ~/media/video
+#mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
 mkdir -p ~/docker
 mkdir -p ~/docker/jellyfin
@@ -19,3 +18,5 @@ mkdir -p ~/docker/radarr
 mkdir -p ~/docker/qbittorrent
 curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
 sed -i "s|username|$username|g" ~/docker/compose.yaml
+docker install
+compose up
