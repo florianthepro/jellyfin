eb02b50d6df17900130ddbb337f579d2317a91c2
florianthepro <florianthepro@icloud.com>
2026-03-26 11:30:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,22 +12,8 @@ mkdir -p ~/media/music
 mkdir -p ~/media/video #mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
 
-mkdir -p ~/docker/jellyfin
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/jellyfin/compose.yaml -o ~/docker/jellyfin/compose.yaml
-sed -i "s|username|$username|g" ~/docker/jellyfin/compose.yaml
+mkdir -p ~/docker
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
+sed -i "s|username|$username|g" ~/docker/compose.yaml
 
-#mkdir -p ~/docker/seerr
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/seerr/compose.yaml -o ~/docker/seerr/compose.yaml
-#sed -i "s|username|$username|g" ~/docker/seerr/compose.yaml
-
-#mkdir -p ~/docker/sonarr
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
-#sed -i "s|username|$username|g" ~/docker/sonarr/compose.yaml
-
-#mkdir -p ~/docker/radarr
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
-#sed -i "s|username|$username|g" ~/docker/radarr/compose.yaml
-
-#mkdir -p ~/docker/qbittorrent
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/qbittorrent/compose.yaml -o ~/docker/qbittorrent/compose.yaml
-#sed -i "s|username|$username|g" ~/docker/qbittorrent/compose.yaml
+#seerr sonarr radarr qbittorrent
