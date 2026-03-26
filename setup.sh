21ee04fe9d32ea1248e340cc52a323a8a68d2571
florianthepro <florianthepro@icloud.com>
2026-03-26 08:21:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-apt update -y -qq
-apt upgrade -y -qq
+apt update
+apt upgrade
 mkdir -p ~/docker/jellyfin
 mkdir -p ~/docker/sonarr
 mkdir -p ~/docker/radarr
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/jellyfin/compose.yaml -o ~/docker/jellyfin/compose.yaml
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
