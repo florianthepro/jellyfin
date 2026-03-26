e414e57b5159e2929906a971b5e1ba3e9692e771
florianthepro <florianthepro@icloud.com>
2026-03-26 09:20:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,14 +7,16 @@ userpass="Password123!"
 
 apt update
 apt upgrade
+
 mkdir -p ~/media/music
-mkdir -p ~/media/video
+mkdir -p ~/media/video # mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
-# mkdir -p ~/media/video/series-a/season00/s01E01.mkv
-# mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
+
 mkdir -p ~/docker/jellyfin
-mkdir -p ~/docker/sonarr
-mkdir -p ~/docker/radarr
 curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/jellyfin/compose.yaml -o ~/docker/jellyfin/compose.yaml
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
+
+#mkdir -p ~/docker/sonarr
+#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
+
+#mkdir -p ~/docker/radarr
+#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
