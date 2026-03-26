86e4ded183d3596a6035c70ec42bcbaf04096669
florianthepro <florianthepro@icloud.com>
2026-03-26 11:51:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,11 @@ userpass="Password123!"
 mkdir -p ~/media/music
 mkdir -p ~/media/video #mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
-
 mkdir -p ~/docker
+mkdir -p ~/docker/jellyfin
+mkdir -p ~/docker/seerr
+mkdir -p ~/docker/sonarr
+mkdir -p ~/docker/radarr
+mkdir -p ~/docker/qbittorrent
 curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
 sed -i "s|username|$username|g" ~/docker/compose.yaml
-
-#seerr sonarr radarr qbittorrent
