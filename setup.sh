f97b06852c82aeffe4d3e55126cbb5feba18fb85
florianthepro <florianthepro@icloud.com>
2026-03-26 08:51:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 apt update
 apt upgrade
+mkdir -p ~/media/music
+mkdir -p ~/media/video
+mkdir -p ~/media/books
+# mkdir -p ~/media/video/series-a/season00/s01E01.mkv
+# mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/docker/jellyfin
 mkdir -p ~/docker/sonarr
 mkdir -p ~/docker/radarr
