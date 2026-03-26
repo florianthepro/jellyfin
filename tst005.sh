c8337cb00078846494e982be79812c00bdb4440b
florianthepro <florianthepro@icloud.com>
2026-03-26 15:03:21 +0100

    Update and rename tst003.sh to tst005.sh

--- tst005.sh
+++ tst005.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 set -euo pipefail
-
 ask() {
 printf "%s" "$1" >/dev/tty
 IFS= read -r REPLY </dev/tty
 }
-
 sudo apt update -qq -y
 sudo apt upgrade -qq -y
-
 #===== intallation =====
-
 echo "goto https://login.tailscale.com/admin/acls/file"
 echo "press Edit anyway..."
 echo "add"
@@ -22,24 +18,16 @@ cat <<'END'
 		}
 	]
 END
-
 ask "done? "
-
-curl -sSL https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker.sh | sudo bash
-sudo usermod -aG docker "$(whoami)"
 username="$(whoami)"
 userid="$(id -u)"
 groupid="$(id -g)"
-
 ask "Please enter your Password: "
 userpass="$REPLY"
-echo "$userpass"
 echo "goto https://login.tailscale.com/admin/settings/keys"
 echo "press Generate auth key..."
-
 ask "Enter your Auth Key: "
 tsauthkey="$REPLY"
-
 mkdir -p ~/media/music
 mkdir -p ~/media/video
 mkdir -p ~/media/books
@@ -52,8 +40,8 @@ mkdir -p ~/docker/qbittorrent
 sudo curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
 sed -i "s/fill-usr/$username/g" ~/docker/compose.yaml
 sed -i "s/fill-key/$tsauthkey/g" ~/docker/compose.yaml
-
+#===== docker =====
+sudo usermod -aG docker "$(whoami)"
 #===== setup =====
-
 docker compose -f /home/$username/docker/compose.yaml up -d
 tailscale funnel 8096 on
