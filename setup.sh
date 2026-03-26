8a43881556101754ef933e09c0b162113987244c
florianthepro <florianthepro@icloud.com>
2026-03-26 15:39:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,59 +1,87 @@
 #!/bin/sh
+clear
 set -euo pipefail
+cd /home/$(whoami)
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo rm -rf *
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
 
 ask() {
 printf "%s" "$1" >/dev/tty
 IFS= read -r REPLY </dev/tty
 }
 
-ask "Bitte Eingabe: "
-var="$REPLY"
-
-sudo apt update
-sudo apt upgrade
-
-#===== intallation =====
-
-echo "goto https://login.tailscale.com/admin/acls/file"
-echo "press Edit anyway..."
-echo "add"
+clear
 cat <<'END'
+>goto https://login.tailscale.com/admin/acls/file
+
+>press "Edit anyway..."
+
+>add:
+
+====================
 	"nodeAttrs": [
 		{
 			"target": ["autogroup:member"],
 			"attr":   ["funnel"]
 		}
 	]
+===================
+
 END
+ask "done? "
 
-printf "Please enter done: " >&2
-IFS= read -r donener
-
-curl -sSL https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker.sh | sudo bash
-sudo usermod -aG docker "$(whoami)"
+clear
 username="$(whoami)"
 userid="$(id -u)"
 groupid="$(id -g)"
-printf "Please enter your Password: " >/dev/tty
-IFS= read -r userpass </dev/tty
-echo "goto https://login.tailscale.com/admin/settings/keys"
-echo "press Generate auth key..."
-printf "Enter your Auth Key: " >/dev/tty
-IFS= read -r tsauthkey </dev/tty
+
+clear
+ask "Please enter your Password: "
+userpass="$REPLY"
+
+clear
+cat <<'END'
+>goto "https://login.tailscale.com/admin/settings/keys"
+
+>press "Generate auth key..."
+
+END
+ask "Enter your Auth Key: "
+tsauthkey="$REPLY"
+
+clear
 mkdir -p ~/media/music
 mkdir -p ~/media/video
 mkdir -p ~/media/books
-mkdir ~/docker
+mkdir -p ~/docker
 mkdir -p ~/docker/jellyfin
 mkdir -p ~/docker/seerr
 mkdir -p ~/docker/sonarr
 mkdir -p ~/docker/radarr
 mkdir -p ~/docker/qbittorrent
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
+
+sudo curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/compose.yaml -o ~/docker/compose.yaml
 sed -i "s/fill-usr/$username/g" ~/docker/compose.yaml
 sed -i "s/fill-key/$tsauthkey/g" ~/docker/compose.yaml
 
+#===== docker =====
+sudo apt update -qq -y
+sudo apt install -qq -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -qq -y
+sudo apt install -qq -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker "$(whoami)"
+clear
 #===== setup =====
-
 docker compose -f /home/$username/docker/compose.yaml up -d
-tailscale funnel 8096 on
+
+#tailscale funnel 8096 on
+#===== end =====
+clear
+cat ./docker/compose.yaml
+ask "done? "
