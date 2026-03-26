36e7ecf71a68e2a4b55fca5daaf8be83779f1215
florianthepro <florianthepro@icloud.com>
2026-03-26 13:12:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,26 +14,16 @@ prompt() {
 username="$(whoami)"
 userid="$(id -u)"
 groupid="$(id -g)"
-userpass="$(prompt "Bitte Password eingeben: ")"
-userpass="Password123!"
-echo enter tailsclae key get from https://login.tailscale.com/admin/settings/keys Auth keys
-tsauthkey="key"
-go to 
-https://login.tailscale.com/admin/acls/file
-"""""
-add
-	"nodeAttrs": [
-		{
-			"target": ["autogroup:member"],
-			"attr":   ["funnel"]
-		}
-	]
-  """""
+#echo "Hello, World!"
+userpass="$(prompt "Please enter your Password: ")"
+echo "goto https://login.tailscale.com/admin/settings/keys"
+echo "press Generate auth key..."
+tsauthkey="$(prompt "enter your Auth Key: ")"
+
 apt update
 apt upgrade
 mkdir -p ~/media/music
 mkdir -p ~/media/video
-#mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
 mkdir -p ~/docker
 mkdir -p ~/docker/jellyfin
@@ -46,5 +36,3 @@ sed -i "s|fill-usr|$username|g" ~/docker/compose.yaml
 sed -i "s|fill-key|$tsauthkey|g" ~/docker/compose.yaml
 docker install
 compose up
-#sudo tailscale funnel 8096 on
-#sudo tailscale funnel on 8096
