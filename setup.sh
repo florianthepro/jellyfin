4822f2eb79fe7ddec3e6094b10370a6275da189c
florianthepro <florianthepro@icloud.com>
2026-03-26 09:31:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#curl -sSL https://raw.githubusercontent.com/florianthepro/jellyfin/main/setup.sh | sudo bash
+#curl -sSL https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/setup.sh | sudo bash
 username="$(whoami)"
 userid="$(id -u)"
 groupid="$(id -g)"
@@ -12,18 +12,18 @@ mkdir -p ~/media/music
 mkdir -p ~/media/video #mkdir -p ~/media/video/series-a/season00/s01E01.mkv & mkdir -p ~/media/video/movie-name/data like mp3 mkv etc
 mkdir -p ~/media/books
 
-mkdir -p ~/docker/jellyfin
-curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/jellyfin/compose.yaml -o ~/docker/jellyfin/compose.yaml
-sed -i "s|username|$username|g" ~/docker/jellyfin/compose.yaml
+mkdir -p ~/docker/jellyfin-enhanced-setup
+curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/jellyfin-enhanced-setup/compose.yaml -o ~/docker/jellyfin-enhanced-setup/compose.yaml
+sed -i "s|username|$username|g" ~/docker/jellyfin-enhanced-setup/compose.yaml
 
 #mkdir -p ~/docker/sonarr
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
+#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/sonarr/compose.yaml -o ~/docker/sonarr/compose.yaml
 #sed -i "s|username|$username|g" ~/docker/sonarr/compose.yaml
 
 #mkdir -p ~/docker/radarr
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
+#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/radarr/compose.yaml -o ~/docker/radarr/compose.yaml
 #sed -i "s|username|$username|g" ~/docker/radarr/compose.yaml
 
 #mkdir -p ~/docker/qbittorrent
-#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin/main/docker/qbittorrent/compose.yaml -o ~/docker/qbittorrent/compose.yaml
+#curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/docker/qbittorrent/compose.yaml -o ~/docker/qbittorrent/compose.yaml
 #sed -i "s|username|$username|g" ~/docker/qbittorrent/compose.yaml
