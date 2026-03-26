b95009b55d8a3001706ef1451eebe595696c8be8
florianthepro <florianthepro@icloud.com>
2026-03-26 15:04:06 +0100

    Update tst005.sh

--- tst005.sh
+++ tst005.sh
@@ -41,6 +41,13 @@ sudo curl -L https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-s
 sed -i "s/fill-usr/$username/g" ~/docker/compose.yaml
 sed -i "s/fill-key/$tsauthkey/g" ~/docker/compose.yaml
 #===== docker =====
+sudo apt update -qq -y
+sudo apt install -qq -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -qq -y
+sudo apt install -qq -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker "$(whoami)"
 #===== setup =====
 docker compose -f /home/$username/docker/compose.yaml up -d
