bf1ecfc99c9189a77f34a5911eb87ed6a5d3c04c
florianthepro <florianthepro@icloud.com>
2026-03-26 17:40:47 +0100

    Update setup-jellyfin-tst.sh

--- setup-jellyfin-tst.sh
+++ setup-jellyfin-tst.sh
@@ -1,1 +1,9 @@
+#===== setup =====
+docker compose -f /home/$username/docker/compose.yaml up -d
 
+#tailscale funnel 8096 on
+#===== end =====
+clear
+cat ./docker/compose.yaml
+ask "done? "
+curl -sSL https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/setup-jellyfin.sh | bash
