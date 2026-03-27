54b8268f884a326207ed0c4025bf4ba6fdf4e0ed
florianthepro <florianthepro@icloud.com>
2026-03-27 12:02:42 +0100

    Update setup-jellyfin.sh

--- setup-jellyfin.sh
+++ setup-jellyfin.sh
@@ -26,24 +26,18 @@ jellyfin_admin_password="$REPLY"
 jellyfin_remote_access="true"
 jellyfin_remote_upnp="false"
 
-echo "Warte auf Jellyfin‑Wizard..."
-until curl -fsS "http://127.0.0.1:8096/System/Info/Public" >/dev/null 2>&1; do
-  sleep 1
-done
+echo "Warte auf Jellyfin‑Wizard..." until curl -fsS "http://127.0.0.1:8096/System/Info/Public"; do sleep 1 done
 
 curl -fsS -X POST "$jellyfin_url/Startup/Configuration" \
 -H "Content-Type: application/json" \
 -d "{\"MetadataCountryCode\":\"$jellyfin_metadata_country\",\"PreferredMetadataLanguage\":\"$jellyfin_metadata_language\",\"UICulture\":\"$jellyfin_language\"}" \
->/dev/null
 
 curl -fsS -X POST "$jellyfin_url/Startup/User" \
 -H "Content-Type: application/json" \
 -d "{\"Name\":\"$jellyfin_admin_user\",\"Password\":\"$jellyfin_admin_password\"}" \
->/dev/null
 
 curl -fsS -X POST "$jellyfin_url/Startup/RemoteAccess" \
 -H "Content-Type: application/json" \
 -d "{\"EnableRemoteAccess\":$jellyfin_remote_access,\"EnableAutomaticPortMapping\":$jellyfin_remote_upnp}" \
->/dev/null
 
-curl -fsS -X POST "$jellyfin_url/Startup/Complete" >/dev/null
+curl -fsS -X POST "$jellyfin_url/Startup/Complete"
