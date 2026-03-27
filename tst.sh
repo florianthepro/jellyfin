0fddc098f89451e37585770cff4884562967dbfd
florianthepro <florianthepro@icloud.com>
2026-03-27 16:16:24 +0100

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,1 +1,1 @@
-
+curl -H 'Cache-Control: no-cache, no-store, must-revalidate' -H 'Pragma: no-cache' -H 'Expires: 0' -sSL "https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/reset.sh?$(date +%s)" | sudo bash && curl -H 'Cache-Control: no-cache, no-store, must-revalidate' -H 'Pragma: no-cache' -H 'Expires: 0' -sSL "https://raw.githubusercontent.com/florianthepro/jellyfin-enhanced-setup/main/install.sh?$(date +%s)" | bash
