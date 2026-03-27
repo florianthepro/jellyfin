880fc8b5ba8695e2c41760c18ef4a20a9715c078
florianthepro <florianthepro@icloud.com>
2026-03-27 18:01:25 +0100

    Update and rename tstst.sh to tst2.sh

---
+++ tst2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd /home/$(whoami)
+
+ask() {
+printf "%s" "$1" >/dev/tty
+IFS= read -r REPLY </dev/tty
+}
+
+username="admin"
+ask "Enter your Admin Password: "
+userpass="$REPLY"
+
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/Configuration" -H "Content-Type: application/json" -d '{"MetadataCountryCode":"DE","PreferredMetadataLanguage":"de","UICulture":"de-DE"}'
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/User" -H "Content-Type: application/json" -d "{\"Name\":\"$username\",\"Password\":\"$userpass\"}"
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/RemoteAccess" -H "Content-Type: application/json" -d '{"EnableRemoteAccess":true,"EnableAutomaticPortMapping":false}'
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/Complete" -H "Content-Type: application/json" -d '{}'
