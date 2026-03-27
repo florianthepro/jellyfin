d18537b0f3145f73da331fe8d04c4b186229bcda
florianthepro <florianthepro@icloud.com>
2026-03-27 18:04:49 +0100

    Update tst4.sh

--- tst4.sh
+++ tst4.sh
@@ -9,9 +9,6 @@ IFS= read -r REPLY </dev/tty
 }
 
 addr=$(ip -4 route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") {print $(i+1); exit}}')
-username="admin"
-ask "Enter your Admin Password: "
-userpass="$REPLY"
 
 curl -fsS -X POST "http://$addr:8096/Startup/Configuration" -H "Content-Type: application/json" -d '{"MetadataCountryCode":"DE","PreferredMetadataLanguage":"de","UICulture":"de-DE"}'
 curl -fsS -X POST "http://$addr:8096/Startup/User" -H "Content-Type: application/json" -d "{\"Name\":\"admin\",\"Password\":\"Password123!\"}"
