4ad17c8a46064439c705e373b5012d306ea5d79a
florianthepro <florianthepro@icloud.com>
2026-03-27 18:00:05 +0100

    Update tsts.sh

--- tsts.sh
+++ tsts.sh
@@ -12,7 +12,7 @@ username="$(whoami)"
 ask "Enter your Admin Password: "
 userpass="$REPLY"
 
-curl -fsS -X POST "http://127.0.0.1:8096/Startup/Configuration" -H "Content-Type: application/json" -d '{"MetadataCountryCode":"DE","PreferredMetadataLanguage":"de","UICulture":"de-DE"}'
-curl -fsS -X POST "http://127.0.0.1:8096/Startup/User" -H "Content-Type: application/json" -d "{\"Name\":\"$username\",\"Password\":\"$userpass\"}"
-curl -fsS -X POST "http://127.0.0.1:8096/Startup/RemoteAccess" -H "Content-Type: application/json" -d '{"EnableRemoteAccess":true,"EnableAutomaticPortMapping":false}'
-curl -fsS -X POST "http://127.0.0.1:8096/Startup/Complete" -H "Content-Type: application/json" -d '{}'
+sudo curl -fsS -X POST "http://127.0.0.1:8096/Startup/Configuration" -H "Content-Type: application/json" -d '{"MetadataCountryCode":"DE","PreferredMetadataLanguage":"de","UICulture":"de-DE"}'
+sudo curl -fsS -X POST "http://127.0.0.1:8096/Startup/User" -H "Content-Type: application/json" -d "{\"Name\":\"$username\",\"Password\":\"$userpass\"}"
+sudo curl -fsS -X POST "http://127.0.0.1:8096/Startup/RemoteAccess" -H "Content-Type: application/json" -d '{"EnableRemoteAccess":true,"EnableAutomaticPortMapping":false}'
+sudo curl -fsS -X POST "http://127.0.0.1:8096/Startup/Complete" -H "Content-Type: application/json" -d '{}'
