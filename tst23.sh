a8508f94af69a7b249053ea8aa5468f3df401cf8
florianthepro <florianthepro@icloud.com>
2026-03-27 09:50:45 +0100

    Update and rename tst.sh to tst23.sh

--- tst23.sh
+++ tst23.sh
@@ -55,9 +55,12 @@ sudo apt update -qq -y
 sudo apt install -qq -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker "$(whoami)"
 
-#DOCKER_GID=$(getent group docker | cut -d: -f3)
-#DOCKER_UID=$(getent passwd "$username" | cut -d: -f3)
-#DOCKER_GID=$(getent passwd "$username" | cut -d: -f4)
+DOCKER_GID=$(getent group docker | cut -d: -f3) | bash -x
+echo "$DOCKER_GID" | bash -x
+DOCKER_UID=$(getent passwd "$username" | cut -d: -f3) | bash -x
+echo "$DOCKER_UID" | bash -x
+DOCKER_GID=$(getent passwd "$username" | cut -d: -f4) | bash -x
+echo "$DOCKER_GID" | bash -x
 #sudo chown -R "$DOCKER_UID:$DOCKER_GID" /home/$username/docker
 #sudo chmod -R u+rwX /home/$username/docker
 
