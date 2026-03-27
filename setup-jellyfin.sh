5fe18f4a7eca3abdb0463e4ee3f09207586a4908
florianthepro <florianthepro@icloud.com>
2026-03-27 15:50:50 +0100

    Update and rename setup-jellyfin2.sh to setup-jellyfin.sh

---
+++ setup-jellyfin.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+cd /home/$(whoami)
+
+ask() {
+printf "%s" "$1" >/dev/tty
+IFS= read -r REPLY </dev/tty
+}
+
+addr=$(ip -4 route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") {print $(i+1); exit}}')
+username="$(whoami)"
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/Configuration" -H "Content-Type: application/json" -d '{"MetadataCountryCode":"DE","PreferredMetadataLanguage":"de","UICulture":"de-DE"}'
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/User" -H "Content-Type: application/json" -d "{\"Name\":\"$username\",\"Password\":\"$userpasw\"}"
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/RemoteAccess" -H "Content-Type: application/json" -d '{"EnableRemoteAccess":true,"EnableAutomaticPortMapping":false}'
+curl -fsS -X POST "http://127.0.0.1:8096/Startup/Complete" -H "Content-Type: application/json" -d '{}'
